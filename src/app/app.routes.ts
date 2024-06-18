18ac69d8466e0e3a4e50540a63f66481547ee55c
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2024-06-18 19:47:28 +0200

    routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { UsersComponent } from './components/users/users.component';
 import { authGuard } from './auth.guard';
 
 const isLoggedIn = () => {
@@ -14,5 +17,9 @@ const isLoggedIn = () => {
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  { path: '', redirectTo: isLoggedIn, pathMatch: 'full' }
+  { path: 'projects', component: ProjectsComponent, canActivate: [authGuard] },
+  { path: 'tasks', component: TasksComponent, canActivate: [authGuard] },
+  { path: 'users', component: UsersComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: isLoggedIn, pathMatch: 'full' },
+  { path: '**', redirectTo: isLoggedIn }
 ];
